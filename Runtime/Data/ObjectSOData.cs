dfd8df3a36db470e62d8333c7903dcae154340a5
bittinkerer <bittinkerer@gmail.com>
2023-09-29 07:53:22 -0700

    Add new SO object

---
+++ Runtime/Data/ObjectSOData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Packages.Estenis.ScriptableObjectsData_
+{
+    [CreateAssetMenu(menuName = "SOData/ObjectSOData")]
+    public class ObjectSOData : BaseSOData<object>
+    {
+    }
+}
