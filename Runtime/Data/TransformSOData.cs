4f0de45ac6b89ef43a31958d503a75e6389ac92c
bittinkerer <bittinkerer@gmail.com>
2024-03-24 15:28:35 -0700

    Add Transform Data Type

---
+++ Runtime/Data/TransformSOData.cs
@@ -0,0 +1,10 @@
+﻿using Packages.Estenis.ScriptableObjectsData_;
+using UnityEngine;
+
+namespace Packages.com.estenis.scriptableobjects_data.Runtime.Data
+{
+    [CreateAssetMenu(menuName = "SOData/TransformSOData")]
+    public class TransformSOData : BaseSOData<Transform>
+    {
+    }
+}
