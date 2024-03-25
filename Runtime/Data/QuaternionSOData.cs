19b674379cc6ad4728c7971d9892eb36ce83bacf
bittinkerer <bittinkerer@gmail.com>
2024-03-24 20:58:57 -0700

    Add new SO data for quaternion

---
+++ Runtime/Data/QuaternionSOData.cs
@@ -0,0 +1,10 @@
+﻿using Packages.Estenis.ScriptableObjectsData_;
+using UnityEngine;
+
+namespace Packages.com.estenis.scriptableobjects_data.Runtime.Data
+{
+    [CreateAssetMenu(menuName = "SOData/QuaternionSOData")]
+    public class QuaternionSOData : BaseSOData<Quaternion>
+    {
+    }
+}
